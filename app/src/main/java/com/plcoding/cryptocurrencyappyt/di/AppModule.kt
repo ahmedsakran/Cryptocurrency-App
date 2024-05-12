e293388b5d07234d48024ea1017b5ecf8b1fe37f
Ahmed Fayez <developer_fayez@yahoo.com>
2024-05-12 20:52:48 +0300

    first commit

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.plcoding.cryptocurrencyappyt.di
+
+import com.plcoding.cryptocurrencyappyt.common.Constants
+import com.plcoding.cryptocurrencyappyt.data.remote.CoinPaprikaApi
+import com.plcoding.cryptocurrencyappyt.data.repository.CoinRepositoryImpl
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+// Instances of components provided in the modules live throughout the application
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+// make sure that there i only a single instance throw out the whole live time
+    @Singleton
+    fun providePaprikaApi():CoinPaprikaApi{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api:CoinPaprikaApi):CoinRepository{
+        return CoinRepositoryImpl(api)
+    }
+}
